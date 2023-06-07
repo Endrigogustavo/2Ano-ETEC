95b5872dc9b0222be5ea6e4423597d45945e7546
Endrigogustavo <endrigogustavo2006@gmail.com>
2023-06-07 10:56:57 -0300

    m

--- ETEC/PW 2/Atvs/8-Aula/BD_Autoria/PHP/Listar2.php
+++ ETEC/PW 2/Atvs/8-Aula/BD_Autoria/PHP/Listar2.php
@@ -14,7 +14,7 @@
     $p = new Produto();
     $pro_bd=$p-> listar();
     ?>
-    <b> Id &nbsp;&nbsp;&nbsp;&nbsp; Nome &nbsp;&nbsp;&nbsp;&nbsp; Estoque</b>
+    <b> Id &nbsp;&nbsp;&nbsp;&nbsp; Nome &nbsp;&nbsp;&nbsp;&nbsp; Estoque &nbsp;&nbsp;&nbsp;&nbsp;</b>
     
     <?php
         foreach($pro_bd as $pro_mostrar){
@@ -23,6 +23,7 @@
             <b> <?php echo $pro_mostrar[0]; ?></b>&nbsp;&nbsp;&nbsp;&nbsp;
                 <?php echo $pro_mostrar[1]; ?>    &nbsp;&nbsp;&nbsp;&nbsp;
                 <?php echo $pro_mostrar[2]; ?>
+                <?php echo $pro_mostrar[3]; ?>
             <?php
         }
             echo "<br><br><button><a href = '../index.html'>Voltar </a></button> "; ?>
