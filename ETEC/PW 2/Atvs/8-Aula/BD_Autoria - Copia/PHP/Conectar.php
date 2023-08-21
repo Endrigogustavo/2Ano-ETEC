028b3b3130234c8c615875cf87820ea87ddae048
Endrigogustavo <endrigogustavo2006@gmail.com>
2023-08-21 09:22:07 -0300

    m

---
+++ ETEC/PW 2/Atvs/8-Aula/BD_Autoria - Copia/PHP/Conectar.php
@@ -0,0 +1,34 @@
+<?php
+class Conectar extends PDO{
+    private static $instancia;
+    private $query;
+    private $host = "localhost";
+    private  $usuario = "root";
+    private  $senha = "";
+    private  $db = "bd_autoria";
+
+    public function __construct(){
+        parent:: __construct("mysql:host=$this->host;dbname=$this->db","$this->usuario","$this->senha");
+    }
+
+    public static function getInstance(){
+        if(!isset(self::$instancia)){
+            try{
+                self::$instancia = new Conectar;
+            }catch(Exception $e){
+                echo 'Erro ao conectar';
+                exit();
+            }
+        }
+        return self::$instancia;
+    }
+
+    public function sql($query){
+        $this->getInstance();
+        $this->query = $query;
+        $stmt = self::$instancia->prepare($this->query);
+        $stmt->execute();
+        self::$instancia = null;
+    }
+}
+?>
---
+++ ETEC/PW 2/Atvs/8-Aula/BD_Autoria - Copia/PHP/Lista3.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <center><section>
+    <font face="Century Gothic" size="6"><b>Relação de Produtos Cadastrados</b><br><br></font>
+
+    <?php
+
+    include_once 'Livro.php';
+    $p = new Produto();
+    $pro_bd=$p-> listar();
+    ?>
+    <b> cod &nbsp;&nbsp;&nbsp;&nbsp; Titulo &nbsp;&nbsp;&nbsp;&nbsp; categoria&nbsp;&nbsp;&nbsp;&nbsp; ISBN &nbsp;&nbsp;&nbsp;&nbsp; Idioma&nbsp;&nbsp;&nbsp;&nbsp; Quantidade pag</b>
+    
+    <?php
+        foreach($pro_bd as $pro_mostrar){
+            ?>
+            <br><br>
+            <b> <?php echo $pro_mostrar[0]; ?></b>&nbsp;&nbsp;&nbsp;&nbsp;
+                <?php echo $pro_mostrar[1]; ?>    &nbsp;&nbsp;&nbsp;&nbsp;
+                <?php echo $pro_mostrar[2]; ?>
+                <?php echo $pro_mostrar[3]; ?>
+                <?php echo $pro_mostrar[4]; ?>
+                <?php echo $pro_mostrar[5]; ?>
+            <?php
+        }
+            echo "<br><br><button><a href = '../index.html'>Voltar </a></button> "; ?>
+            
+    </section></center>
+</body>
+</html>
