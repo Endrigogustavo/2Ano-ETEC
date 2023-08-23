bac53fe1686b03dc870901f1bd669b93158c70ad
Endrigogustavo <endrigogustavo2006@gmail.com>
2023-08-23 14:43:12 -0300

    M

---
+++ ETEC/AppCompose/app/src/test/java/com/example/appcompose/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.example.appcompose
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
